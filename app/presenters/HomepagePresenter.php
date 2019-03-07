0a9290bb056cb382eb0d2d34acda91ab44582c4f
Simon Kravec <simonko420@gmail.com>
2019-03-07 21:21:13 +0100

    CLEARED project, ADDED webloader

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,18 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    /** @var \WebLoader\Nette\LoaderFactory @inject */
+    public $webLoader;
+
+    /** @return CssLoader */
+    protected function createComponentCss()
+    {
+        return $this->webLoader->createCssLoader('default');
+    }
+
+    /** @return JavaScriptLoader */
+    protected function createComponentJs()
+    {
+        return $this->webLoader->createJavaScriptLoader('default');
+    }
 }
