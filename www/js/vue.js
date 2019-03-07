0d81f8069bf485fa63a31e741dc16d0882b03339
Simon Kravec <simonko420@gmail.com>
2019-03-07 23:36:35 +0100

    FIRST app

---
+++ www/js/vue.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        newName: '',
+        newAge: '',
+        guys: [
+            {
+                name: 'pivko420',
+                age: '17',
+                cool: '100'
+            }, {
+                name: 'daveloper',
+                age: '18',
+                cool: '10'
+            }
+        ]
+    },
+    methods: {
+        add: function () {
+            this.guys.push({
+                name: this.newName,
+                age: this.newAge,
+                cool: '50'
+            });
+            this.newName = '';
+            this.newAge = '';
+
+            this.newName.focus();
+        },
+        remove: function (guy) {
+            this.guys = this.guys.filter( item => item !== guy );
+        }
+    }
+});
